c8c8b7c3e8e34215cad6ef5d6b2fa3efb64aecfd
Hamza Masood <hamzaa.fsd@gmail.com>
2022-12-06 17:55:03 +0500

    Update port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('./src/db/connectdb.js')
 const botRouter = require('./src/routers/botRouter');
+const Bot = require('./src/model/botModel')
+
 
 // Connecting To Port
 const port = process.env.port || 8080;
@@ -12,6 +14,17 @@ app.use(express.urlencoded({extended: false}))
 
 // Using the botRouter file
 app.use(botRouter);
+
+app.get('/readBot', async (req, res) => {
+    try {
+        const readBot = await Bot.find({})
+        console.log(new Date().toLocaleString() + ' ' + 'Loading To Read Bot...')
+        res.send(readBot) 
+        console.log(new Date().toLocaleString() + ' ' + 'Reading Bot...')    
+    } catch(e) {
+        res.status(500).send(e);
+    }
+})
 
 // listening To Port
 app.listen(port, () => {
