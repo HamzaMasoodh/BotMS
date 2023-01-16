aae84f16bc1fcb8a5799d4cf25d010ec842840de
Hamza Masood <hamzaa.fsd@gmail.com>
2023-01-16 20:26:27 +0500

    Cors Test

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ const port = process.env.port || 8080;
 // Automatically parse incoming JSON to an object so we access it in our request handlers
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Using the botRouter file
 app.use(botRouter);
 
