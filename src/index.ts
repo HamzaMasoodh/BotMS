00c0c3428702037fa731bacd786bef2f67269f7c
Hamza Masood <hamzaa.fsd@gmail.com>
2023-01-16 20:33:54 +0500

    Cors test-2

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,15 @@ const port = process.env.port || 8080;
 // Automatically parse incoming JSON to an object so we access it in our request handlers
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.options("/*", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.status(200).send();
+  });
+  
 app.use(cors({
-  origin: 'http://localhost:3000/',
+  origin: 'https://exquisite-naiad-58c7e6.netlify.app/',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
